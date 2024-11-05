6c932d85fc31396591d1d1751abfddfcddc812a6
Shivaprasad Bilgundi <shivbbilgundi11@gmail.com>
2024-11-05 18:13:53 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <>
+      <div className="flex min-h-screen w-full flex-col items-center justify-center gap-y-4">
+        <h1 className="text-4xl font-bold underline">Hello React...!</h1>
+        <p className="font-medium tracking-wide text-gray-500">
+          React With TypeScript and TailwindCSS
+        </p>
+      </div>
+    </>
+  );
+}
